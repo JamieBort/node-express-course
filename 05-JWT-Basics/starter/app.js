1a1a7a451a4a653f7d9027991e59677dcce91ab5
Jamie Bort <jamiebort@gmail.com>
2026-01-12 20:18:32 -0500

    Finish code for assignment.

--- 05-JWT-Basics/starter/app.js
+++ 05-JWT-Basics/starter/app.js
@@ -1,29 +1,34 @@
 require('dotenv').config();
-require('express-async-errors');
+require("express-async-errors"); // Errors thrown inside async route handlers and rejected promises inside routes or middleware are automatically forwarded to Express’s error handler.
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const mainRouter = require("./routes/main");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(express.json());
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use("/api/v1", mainRouter);
+
+app.use(notFoundMiddleware); // middleware for routes
+app.use(errorHandlerMiddleware); // middleware for routes
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		app.listen(port, () =>
+			// eslint-disable-next-line no-console
+			console.log(`Server is listening on port ${port}...`),
+		);
+	} catch (error) {
+		// eslint-disable-next-line no-console
+		console.log(error);
+	}
 };
 
 start();
--- 05-JWT-Basics/starter/middleware/error-handler.js
+++ 05-JWT-Basics/starter/middleware/error-handler.js
@@ -1,9 +1,13 @@
-const CustomAPIError = require('../errors/custom-error')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(500).send('Something went wrong try again later')
-}
+const { CustomAPIError } = require("../errors"); // *** I midified this. ***
+const { StatusCodes } = require("http-status-codes"); // *** I added this. ***
+const errorHandlerMiddleware = (err, req, res) => {
+	if (err instanceof CustomAPIError) {
+		return res.status(err.statusCode).json({ msg: err.message });
+	}
+	// *** I midified this. ***
+	return res
+		.status(StatusCodes.INTERNAL_SERVER_ERROR)
+		.send("Something went wrong try again later");
+};
 
-module.exports = errorHandlerMiddleware
+module.exports = errorHandlerMiddleware;
